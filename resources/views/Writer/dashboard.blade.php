c9f9c6923395f23f0e078e36b6e3a6781698f6d9
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-08-17 15:47:40 +0300

    feat: add reusable modal component for article deletion with confirmation popup

--- resources/views/Writer/dashboard.blade.php
+++ resources/views/Writer/dashboard.blade.php
@@ -110,7 +110,9 @@
                             @else
                                 <a href="{{route('writer.view_article',$article->slug)}}" class="action-btn show"><i class="fas fa-eye"></i></a>
                                 <a href="{{route('writer.edit_article',$article->article_id)}}" class="action-btn edit"><i class="fas fa-edit"></i></a>
-                                <a class="action-btn delete"><i class="fas fa-trash"></i></a>
+                                <x-delete-confirm
+                                    :article-id="$article->article_id"
+                                    :article-title="$article->title" />
                             @endif
                         </td>
                     </tr>
