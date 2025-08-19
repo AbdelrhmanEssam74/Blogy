4822edc0ea928bbdbb71fd8a45d313e4f39f4e90
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-08-20 02:48:04 +0300

    feat: enhance admin dashboard with statistics and recent articles
    - Added user, category, and article statistics including counts for total, pending, published, draft, and archived articles.
    - Displayed the 4 most recent articles with their associated users.
    - Corrected `user` relationship in `Article` model to refer to `writer_id`.

--- app/Http/Controllers/admin/AdminDashboardController.php
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $totalUsers = User::where("role_id" , '!=', 1)->count();
+        $totalCategories = Category::count();
+        $totalArticles = Article::count();
+        $pendingArticles = Article::where('status', 'pending')->count();
+        $publishedArticles = Article::where('status', 'published')->count();
+        $draftArticles = Article::where('status', 'draft')->count();
+        $archivedArticles = Article::where('status', 'archived')->count();
+        // get recent articles with the user who created it
+        $recentArticles = Article::with('user')->latest()->take(4)->get();
+        return view('admin.dashboard',
+            compact(
+                'totalArticles',
+                'pendingArticles',
+                'publishedArticles',
+                'draftArticles',
+                'archivedArticles',
+                'recentArticles',
+                'totalUsers',
+                'totalCategories',
+            )
+        );
     }
 }
