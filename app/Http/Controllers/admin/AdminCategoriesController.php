35c36a9febba92fe1e53745444a30f5d29404664
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-09-07 14:23:13 +0300

    add edit category functionality and styles in admin panel
    - Implement `edit.blade.php` view with form for editing categories, including status toggle and image preview.
    - Add `update` method in `AdminCategoriesController` for form submission handling and validation.
    - Introduce `edit-category.css` for styling the category edit page.
    - Enhance slug auto-generation and file upload logic with real-time updates and error handling.
    - Include SweetAlert for feedback notifications and ensure UI responsiveness.

--- app/Http/Controllers/admin/AdminCategoriesController.php
+++ app/Http/Controllers/admin/AdminCategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class AdminCategoriesController extends Controller
@@ -13,20 +14,23 @@ class AdminCategoriesController extends Controller
     {
         $categories = Category::paginate(6);
         return view('admin.categories.index',
-        compact('categories'));
+            compact('categories'));
     }
-    public function create(){
+
+    public function create()
+    {
         return view('admin.categories.create');
     }
-    public function store(Request $request){
+
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'name' => 'required|unique:categories|max:255',
             'description' => 'nullable|max:2000',
             'image' => 'required|nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
             'slug' => 'required|nullable|unique:categories|max:255'
         ]);
-        if($request->hasFile('image'))
-        {
+        if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('categories/' . auth()->user()->user_id, 'public');
             $validated['image'] = $imagePath;
         }
@@ -34,9 +38,63 @@ class AdminCategoriesController extends Controller
         Alert::success('Success', 'Category created successfully');
         return redirect()->route('admin.categories');
     }
+
     public function edit($id)
     {
         $category = Category::findOrFail($id);
         return view('admin.categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, $category_id)
+    {
+        // get writer id from session
+        // Logic to update an article by ID
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'max:255',
+                'string',
+                Rule::unique('categories')->ignore($category_id, 'category_id'),
+            ],
+            'description' => 'nullable|max:2000',
+            'new_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
+            'slug' => [
+                'required',
+                'max:255',
+                'string',
+                Rule::unique('categories')->ignore($category_id, 'category_id'),
+            ]
+        ]);
+
+        $category = Category::findOrFail($category_id);
+        $category->name = $validated['name'];
+        if ($request->has('description')) {
+            $category->description = $validated['description'];
+        }
+        $category->slug = $validated['slug'];
+        if ($request->has('active') && $request->active === 'on') {
+            $category->active = 1;
+        } else {
+            $category->active = 0;
+        }
+        if ($request->hasFile('new_image')) {
+            if ($category->image) {
+                $oldImagePath = 'storage/' . $category->image;
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+            }
+            // store the new image
+            $imagePath = $request->file('new_image')->store('categories/' . auth()->user()->user_id, 'public');
+            $category->image = $imagePath;
+        }
+        // Check if any attribute changed
+        if (!$category->isDirty()) {
+            Alert::info('No Changes', 'No updates were made to the Category.');
+            return redirect()->back();
+        }
+        $category->save();
+        Alert::success('Success', 'Category updated successfully');
+        return redirect()->back();
     }
 }
