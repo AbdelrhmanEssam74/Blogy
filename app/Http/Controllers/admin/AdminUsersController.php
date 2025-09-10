1cc0a5f46dcce623e5f0f62e00d78c2ebf130bdb
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-09-10 03:26:53 +0300

    fetch users with role and writer profile in AdminUsersController index method

--- app/Http/Controllers/admin/AdminUsersController.php
+++ app/Http/Controllers/admin/AdminUsersController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
 {
-    public function index(){
-        return view('admin.users.index');
+    public function index()
+    {
+        $users = User::with(['role', 'writer_profile'])->get();
+        return view('admin.users.index', compact('users'));
     }
 }
